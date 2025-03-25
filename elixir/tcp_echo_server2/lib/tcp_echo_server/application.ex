b4e70c35a7009d862a9465a0272e4eaa7d30117a
reign <reignofmiracle@gmail.com>
2025-03-25 11:08:15 +0900

    update

---
+++ elixir/tcp_echo_server2/lib/tcp_echo_server/application.ex
@@ -0,0 +1,13 @@
+defmodule TCPEchoServer.Application do
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {TCPEchoServer.Accepter, port: 4000}
+    ]
+
+    opts = [strategy: :one_for_one, name: TCPEchoServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
